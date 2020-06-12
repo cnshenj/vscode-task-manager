c3558785ef9b1981ad5581637c7f2bc447bf5760
Zhang Shen <zhangs@microsoft.com>
2020-06-12 14:13:00 -0700

    Prevent Node.js globals mocking by webpack

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ const path = require("path");
 /** @type {import("webpack").Configuration} */
 const config = {
     target: "node",
+    node: false,
     entry: "./src/extension.ts",
     output: {
         path: path.resolve(__dirname, "dist"),
